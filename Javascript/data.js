1bb9cdcfe144cc964762b475968ce9072afca1a3
nicobengamma <kikecarc17@gmail.com>
2022-06-02 15:03:06 -0300

    .

--- Javascript/data.js
+++ Javascript/data.js
@@ -1,7 +1,21 @@
+
+
 function buscar1() {
   let search = document.getElementById("search").value;
-  let lang = "es";
-  let Url = `https://api.giphy.com/v1/gifs/search?api_key=CsZ2k1XvRYvEuXOG8kxi1E3Ogm7d0ZEI&q=${search}&limit=25&offset=0&rating=g&lang=${lang}`;
+  let lang = document.getElementById("languaje1").value;
 
-  console.log(search);
+  let apiK = `https://api.giphy.com/v1/gifs/search?api_key=CsZ2k1XvRYvEuXOG8kxi1E3Ogm7d0ZEI&q=${search}&limit=25&offset=0&rating=g&lang=${lang}`;
+  
+  console.log(apiK)
+
+  fetch(apiK)
+  .then(response => response.json())
+  .then(data => {
+    let image = data.data[0].images.original.url
+    console.log(image)
+
+  } 
+    )
+
+  
 }
